54596a4cef5c1ecabde6ef5c1e864c9dd450e148
Spencer Cambern <cambern134@gmail.com>
2020-01-25 07:37:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma (phrase)
-  if p = "I LOVE YOU GRANDMA!"
+  if phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU PUMPKIN!"
   elsif p ≠ phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
