7cb71b881d8d21314f64b972bc91e51cb8d918bb
Spencer Cambern <cambern134@gmail.com>
2020-01-25 07:47:32 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require_relative '../grandma.rb'
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = false
+  config.run_all_when_everything_filtered = true
   config.filter_run :focus
 
   # Run specs in random order to surface order dependencies. If you find an
