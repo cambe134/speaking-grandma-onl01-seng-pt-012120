48197e460c64df890ba23f07826cf614bac486ef
Spencer Cambern <cambern134@gmail.com>
2020-01-25 08:40:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,9 @@
 def speak_to_grandma(phrase)
  if phrase == "I LOVE YOU GRANDMA!"
   "I LOVE YOU TOO PUMPKIN!"
-elsif phrase == "WHAT DID YOU EAT TODAY?" || "WHAT?"
-  "NO, NOT SINCE 1938!"
 elsif phrase ≠ phrase.upcase()
     "HUH?! SPEAK UP, SONNY!"
+    else 
+      "NO, NOT SINCE 1938!"
   end
 end
